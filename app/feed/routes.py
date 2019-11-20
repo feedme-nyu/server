dbd01bae635b46704dd613ebb1ccbab487672258
pholoubek <peterholoubekjr@gmail.com>
2019-11-19 22:46:56 -0500

    3. commit -> added api pref + find_restaurant() func

--- app/feed/routes.py
+++ app/feed/routes.py
@@ -4,3 +4,14 @@ from flask import jsonify, request
 @bp.route("/try")
 def try_out():
     return jsonify("try_out()")
+    
+@bp.route("/find_restaurant/", methods=['GET', 'POST'])
+def find_restaurant():
+    coords = request.args.to_dict()
+    x = coords["x"]
+    y = coords["y"]
+    print("coords:", x, y)
+    return 'success', 200
+    
+def perform_something_with_api_key(params):
+     request.get(url, apikey)   
